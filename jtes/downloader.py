e7a4546b1ee256206c8c6cd1a31af9a46343e82d
Thor77 <thor77@thor77.org>
2017-03-13 21:59:42 +0100

    Refactor download_episodes
    to return Episode-objects with path to storage-location of file.

--- jtes/downloader.py
+++ jtes/downloader.py
@@ -21,9 +21,24 @@ def download_episodes(episodes, target_path):
     '''
     outtmpl = join(target_path, '%(title)s.%(ext)s')
     YTDL_OPTIONS['outtmpl'] = outtmpl
+
+    downloaded_episodes = []
     with youtube_dl.YoutubeDL(YTDL_OPTIONS) as ytdl:
         for episode in episodes:
-            ytdl.extract_info(episode.path, extra_info={
-                'episode': episode
-            })
-            yield episode
+            # first extract mix-url from JedenTagEinSet
+            jtes_info = ytdl.extract_info(episode.path, process=False)
+            if len(jtes_info['entries']) <= 0:
+                logger.warning('No download found for %s', episode.path)
+            mix_url = jtes_info['entries'][0]['url']  # only download first
+
+            # download mix
+            def save_episode(progress):
+                if progress['status'] != 'finished':
+                    return
+                downloaded_episodes.append(
+                    episode._replace(path=progress['filename'])
+                )
+            ytdl.add_progress_hook(save_episode)
+            ytdl.extract_info(mix_url)
+            ytdl._progress_hooks.remove(save_episode)
+    return downloaded_episodes
